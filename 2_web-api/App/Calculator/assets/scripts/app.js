960826ba8cad7bd56d8b288465a79ed1d1f5f5dd
202216008 <apdlfdldjqtm@gmail.com>
2023-08-02 11:23:34 +0900

    web api - 실전예제1: 리팩토링, 로그이력 기능

--- 2_web-api/App/Calculator/assets/scripts/app.js
+++ 2_web-api/App/Calculator/assets/scripts/app.js
@@ -1,28 +1,50 @@
 
 //index.html에서 활용할 이벤트 처리, 핸들러 ...
 
+//계산 이력을 모아둘 배열
+const logEntries=[];
+
 //현재 게산기에 그려질 숫자
 let currentResult = 0;
 
-
-
-
-
-
-
+//로그 이력을 만드는 함수
+const writeToLog=( operation, prevResult, number, result)=>{
+    //하나의 로그 객체(연산, 이전결과, 연산숫자, 연산결과)
+    const logObject={
+        operation,
+        prevResult, 
+        number,
+        result
+    };
+    logEntries.push(logObject);
+    console.log(logEntries);
+};
 
 //더하기 버튼 이벤트 핸들러
 const addHandler = () =>{
     console.log('+ button click');
+
+    //사용자의 입력값을 읽어오는 함수
+    const getUserNumberInput =()=> + $userInput.value;
+
+    //계산 로그를 만들고 화면에 렌더링하는 함수
+    const createLogAndRenderOutput=(operateMark, originResult, calcNumber)=>{
+        //로그 생성
+        const calcDescriptionLog = `${originResult} ${operateMark} ${calcNumber}`;
+        //화면에 렌더링
+        outputResult(currentResult, calcDescriptionLog);
+    }
+    
     
     //입력창에 입력한 숫자 읽기
-    const enteredNumber = + $userInput.value;    
-    //로그 생성
-    const calcDescriptionLog = `${currentResult} + ${enteredNumber}`;
+    const enteredNumber = getUserNumberInput();      
+    //계산전 값을 백업
+    const originResult = currentResult;
     //실제 게산 결과 반영
     currentResult +=enteredNumber;
-    //화면에 렌더링
-    outputResult(currentResult, calcDescriptionLog);
+    createLogAndRenderOutput('+',originResult, enteredNumber);
+    //로그이력쌓기
+    writeToLog('ADD', originResult, enteredNumber, currentResult);
 
 };
 
@@ -30,29 +52,32 @@ const addHandler = () =>{
 const substractHandler = () =>{
     console.log('- button click');
 
-    const enteredNumber = + $userInput.value;
-    const calcDescriptionLog = `${currentResult} - ${enteredNumber}`;
+    const enteredNumber = getUserNumberInput();
+    const originResult = currentResult;
     currentResult -=enteredNumber;
-    outputResult(currentResult, calcDescriptionLog);
+    outputResult('-',originResult, calcDescriptionLog);
+    writeToLog('SUB', originResult, enteredNumber, currentResult);
 };
 
 //곱하기 multiplyHandler
 const multiplyHandler = () =>{
     console.log('* button click');
 
-    const enteredNumber = + $userInput.value;
+    const enteredNumber = getUserNumberInput();
     const calcDescriptionLog = `${currentResult} * ${enteredNumber}`;
     currentResult *=enteredNumber;
     outputResult(currentResult, calcDescriptionLog);
+    writeToLog('MULTI', originResult, enteredNumber, currentResult);
 };
 //나누기 divideHandler
 const divideHandler = () =>{
     console.log('/ button click');
 
-    const enteredNumber = + $userInput.value;
+    const enteredNumber = getUserNumberInput();
     const calcDescriptionLog = `${currentResult} / ${enteredNumber}`;
     currentResult /=enteredNumber;
     outputResult(currentResult, calcDescriptionLog);
+    writeToLog('DIVID', originResult, enteredNumber, currentResult);
 };
 
 //======================이벤트 핸들러 바인딩======================
