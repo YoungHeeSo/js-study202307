9be7c6395c836faeeac1606f165462e65db1a544
202216008 <apdlfdldjqtm@gmail.com>
2023-08-03 10:12:24 +0900

    web api - 실전예제2: 영화 추가 모달 닫기 기능 -backdrop닫기 이벤트 추가 -취소 닫기 버튼 이벤트 추가

--- 2_web-api/App/Movie/assets/scripts/app.js
+++ 2_web-api/App/Movie/assets/scripts/app.js
@@ -1,44 +1,30 @@
 
 
 
-//모달 엘리먼트
-// const $addMovieModal = document.getElementById("add-modal");
-// const $addMovieButton = document.querySelector("header button");
-
-// //영화추가버튼
-// const $backdrop = document.getElementById("backdrop");
-// //영화 추가 모달안에 있는 확인, 취소버튼
-// const $cancelAddMovieButton = $addMovieModal.querySelector(".btn--passive");
-// const $confirmAddMovieButton = $cancelAddMovieButton.nextElementSibling;
-// const $userInputs = [
-//   ...$addMovieModal.querySelectorAll("input"),
-// ];
-// const [$titleInput, $imgUrlInput, $ratingInput] = $userInputs;
-// const $entryTextSection = document.getElementById('entry-text');
-// const $deleteMovieModal = document.getElementById('delete-modal');
 
 //모달 엘리먼트
 const $addMovieModal = document.getElementById("add-modal");
-const $addMovieButton = document.querySelector("header button");
+const $deleteMovieModal = document.getElementById('delete-modal');
 
 //영화추가버튼
+const $addMovieButton = document.querySelector("header button");
 const $backdrop = document.getElementById("backdrop");
 
 //영화 추가 모달안에 있는 확인, 취소버튼
 const $cancelAddMovieButton = $addMovieModal.querySelector(".btn--passive");
 const $confirmAddMovieButton = $cancelAddMovieButton.nextElementSibling;
 
+//영화 추가 모달 안에 있는 입력엘리먼트들
 const $userInputs = [
   ...$addMovieModal.querySelectorAll("input"),
 ];
 
 const [$titleInput, $imgUrlInput, $ratingInput] = $userInputs;
 const $entryTextSection = document.getElementById('entry-text');
-const $deleteMovieModal = document.getElementById('delete-modal');
 
 const CLASS_VISIBLE='visible'
 
-// 이벤트 핸들러 및 이벤트 바인딩
+// 이벤트 핸들러 및 이벤트 바인딩 //
 
 //영화 추가 모달창을 띄우는 핸들러
 const showMovieModalHandler =e =>{
@@ -46,5 +32,26 @@ const showMovieModalHandler =e =>{
     $backdrop.classList.add(CLASS_VISIBLE);
 };
 
+const closeAddModal=()=>{
+    $backdrop.classList.remove(CLASS_VISIBLE);
+    $addMovieModal.classList.remove(CLASS_VISIBLE);
+}
+
+//백드롭 영역을 클릭하면 모달이 닫히는 핸들러
+const backdropHandler =e=>{
+    closeAddModal();
+};
+
+//영화추가 모달창의 취소버튼이 누르면 모달이 닫히는 핸들러
+const closeMoiveModalHandler=e=>{
+    closeAddModal();
+};
+
 //Add movie버튼 클릭 이벤트
 $addMovieButton.addEventListener('click', showMovieModalHandler);
+
+//backdrop영역 클릭이벤트
+$backdrop.addEventListener('click', backdropHandler);
+
+//add Movie 모달 취소버튼 클릭 이벤트
+$cancelAddMovieButton.addEventListener('click', closeMoiveModalHandler);
