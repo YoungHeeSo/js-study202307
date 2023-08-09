1075cea5b925479b098ae05892eb150d135c515f
202216008 <apdlfdldjqtm@gmail.com>
2023-08-09 10:28:33 +0900

    ajax - 종합실습 - 일정관리 프로그램 - 할 일 삭제하기

--- 3_ajax/TodoList/assets/scripts/app.js
+++ 3_ajax/TodoList/assets/scripts/app.js
@@ -78,6 +78,28 @@ const $addBtn=document.getElementById('add');
 $addBtn.addEventListener('click', addtodoHandler);
 
 // 할 일 삭제 - 삭제 버튼을 누르면 delete요청으로 db.json에서 객체 제거, 렌더링
+const deleteTodoHandler=e=>{
+    if(!e.target.matches('.remove span')) return;
+    console.log('삭제클릭');
+    
+    //특정 할 일을 지우기 위해선, 클릭한 할 일의 id값을 알아야 해
+    //자식은 query 부모는 closest
+    const id = e.target.closest('.todo-list-item').dataset.id;
+    // console.log(id);
+
+    //서버에 삭제 요청하기
+    fetchTodos(`${URL}/${id}`, 'DELETE')
+    .then(res=>{
+        if(res.status===200|| res.status===201){
+            console.log('등ㄹ록');
+        }else{
+            console.log('노등ㄹ록');
+        }
+    });    
+};
+
+$todoList.addEventListener('click', deleteTodoHandler);
+
 // 할 일 체크 기능 - 체크박스를 누르면 put, patch요청으로 db.json에 done값을 반대값으로 수정,changeEvent걸기
 // 할 일 수정 기능 - 초록색버튼을 누르면 수정모드로 진입(인풋창 활성화)
 // 인풋창에 새로운 텍스트를 쓰고 확인을 누르면 put,patch요청이 나가고 db.json에서도 수정
